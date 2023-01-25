31ce980c101f795d1725d1f0b187d798e0a1734d
Davor Komusanac <davor.komusanac@gmail.com>
2023-01-26 00:37:34 +0100

    add firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
