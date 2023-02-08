d82f6fbfb5f32c6493bf40cf3acea8bbe8c8dddc
Davor Komusanac <davor.komusanac@gmail.com>
2023-02-08 19:57:42 +0100

    update data class models for firestore

--- lib/data/firestore/models/firestore_movie_watched_details.dart
+++ lib/data/firestore/models/firestore_movie_watched_details.dart
@@ -5,9 +5,6 @@ class FirestoreMovieWatchedDetails {
     required this.id,
     required this.title,
     required this.posterPath,
-    required this.popularity,
-    required this.voteAverage,
-    required this.releaseDate,
     required this.timestampAddedToFirestore,
     required this.review,
     required this.rating,
@@ -16,9 +13,6 @@ class FirestoreMovieWatchedDetails {
   final int id;
   final String title;
   final String posterPath;
-  final num popularity;
-  final num voteAverage;
-  final String releaseDate;
   final Timestamp timestampAddedToFirestore;
   final String review;
   final num rating;
@@ -28,9 +22,6 @@ class FirestoreMovieWatchedDetails {
         id: data['id'] as int? ?? 0,
         title: data['title'] as String? ?? '',
         posterPath: data['poster_path'] as String? ?? '',
-        popularity: data['popularity'] as num? ?? 0,
-        voteAverage: data['vote_average'] as num? ?? 0,
-        releaseDate: data['release_date'] as String? ?? '',
         timestampAddedToFirestore: data['added_to_list_date'] as Timestamp? ?? Timestamp(0, 0),
         review: data['review'] as String? ?? '',
         rating: data['rating'] as num? ?? 0,
@@ -43,9 +34,6 @@ class FirestoreMovieWatchedDetails {
         'id': id,
         'title': title,
         'poster_path': posterPath,
-        'popularity': popularity,
-        'vote_average': voteAverage,
-        'release_date': releaseDate,
         'added_to_list_date': timestampAddedToFirestore,
         'review': review,
         'rating': rating,
--- lib/data/firestore/models/firestore_movie_watchlist_details.dart
+++ lib/data/firestore/models/firestore_movie_watchlist_details.dart
@@ -5,18 +5,12 @@ class FirestoreMovieWatchlistDetails {
     required this.id,
     required this.title,
     required this.posterPath,
-    required this.popularity,
-    required this.voteAverage,
-    required this.releaseDate,
     required this.timestampAddedToFirestore,
   });
 
   final int id;
   final String title;
   final String posterPath;
-  final num popularity;
-  final num voteAverage;
-  final String releaseDate;
   final Timestamp timestampAddedToFirestore;
 
 // added fromSnapshot and toDocument methods to add movie info to firestore, but only certain fields, not all
@@ -24,9 +18,6 @@ class FirestoreMovieWatchlistDetails {
         id: data['id'] as int? ?? 0,
         title: data['title'] as String? ?? '',
         posterPath: data['poster_path'] as String? ?? '',
-        popularity: data['popularity'] as num? ?? 0,
-        voteAverage: data['vote_average'] as num? ?? 0,
-        releaseDate: data['release_date'] as String? ?? '',
         timestampAddedToFirestore: data['added_to_list_date'] as Timestamp? ?? Timestamp(0, 0),
       );
 
@@ -37,9 +28,6 @@ class FirestoreMovieWatchlistDetails {
         'id': id,
         'title': title,
         'poster_path': posterPath,
-        'popularity': popularity,
-        'vote_average': voteAverage,
-        'release_date': releaseDate,
         'added_to_list_date': timestampAddedToFirestore,
       };
 }
